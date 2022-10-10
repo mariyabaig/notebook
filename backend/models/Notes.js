2818806c05228f9eae5bb672f65cf5147664e52f
mariyabaig <mariya.baig@nagarro.com>
2022-10-10 13:16:13 +0530

    updating node

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,25 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
-  title : {
-    type : String,
-    required : true
+  user: {
+    type: mongoose.Schema.Types.ObjectId, //FOREIGN KEY, to link user to his specific notes
+    ref: "user", //refernce model
   },
-  desc : {
-    type : String,
-    required : true,
-     
+  title: {
+    type: String,
+    required: true,
   },
- tag : {
-    type : String,
-    default : "General"
+  description: {
+    type: String,
+    required: true,
   },
-  date : {
-    type : Date,
-    default: Date.now
-  }
-  
+  tag: {
+    type: String,
+    default: "General",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('notes', UNotesSchema);
+module.exports = mongoose.model("notes", NotesSchema);
