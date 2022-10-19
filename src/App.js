77738e9d4bc6512ba7a12e27e0399a81744ad56c
mariyabaig <mariya.baig@nagarro.com>
2022-10-19 14:47:40 +0530

    notes-card

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-
-import './App.css';
-import Navbar from './components/Navbar';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import About from './components/About';
-import Home from './components/Home';
-import NoteState from './context/notes/NoteState';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
-   <>
-   <NoteState>
-   <Router>
-    <Navbar/>
-    <Switch>
-          <Route exact path="/about">
-            <About/>
-          </Route>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-        </Switch>
-    </Router>
-    </NoteState>
-   </>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
