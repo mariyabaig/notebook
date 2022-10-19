c22fb5bf25ddd1785e33378e70ae98fdc72da384
mariyabaig <mariya.baig@nagarro.com>
2022-10-19 14:48:33 +0530

    note cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,25 @@
-import React from 'react'
+
+
+import React from "react";
+import Notes from "./Notes";
 
 const Home = () => {
+  
   return (
-    <div>Home</div>
+    <>
+  <div className="container my-3">
+<h2>Add a note</h2>
+<div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Email address</label>
+  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+</div>
+<div className="mb-3">
+  <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+  </div>
+  <Notes/>
+  </>
   )
 }
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,6 +2,26 @@
 import NoteContext from "./noteContext";
 import { useState } from "react";
 const NoteState=(props)=>{
+    const notesInitial = [
+        {
+          "_id": "6343bf8df49f87e42646454e",
+          "user": "63354c9ffd654230687ea78b",
+          "title": " First note ",
+          "description": "This is my first note",
+          "tag": "basic",
+          "date": "2022-10-10T06:45:33.342Z",
+          "__v": 0
+        },
+        {
+          "_id": "6343cbe51eb1a9bd13294658",
+          "user": "63354c9ffd654230687ea78b",
+          "title": " First note ",
+          "description": "This is my first note",
+          "tag": "basic",
+          "date": "2022-10-10T07:38:13.327Z",
+          "__v": 0
+        }
+      ]
     // const s1 = {
     //     "name":"Mariya",
     //     "class":"6a"
@@ -18,8 +38,10 @@ const NoteState=(props)=>{
     // }
 
     //{{state,update}}
+
+    const [notes, setNotes] = useState(notesInitial)
  return(
-   <NoteContext.Provider value= {{}}>
+   <NoteContext.Provider value= {{notes,setNotes}}>
     {props.children}
    </NoteContext.Provider>
  )
