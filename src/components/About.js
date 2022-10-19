55a546eb8469f5fbe8e260a92a4ebdb185298d66
mariyabaig <mariya.baig@nagarro.com>
2022-10-19 13:17:20 +0530

    useLocation:active nav

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,16 @@ import React,{useContext,useEffect} from 'react';
 import noteContext from '../context/notes/noteContext';
 
 const About = () => {
-  const a = useContext(noteContext)
-  useEffect(() => {
-      a.update();
-      // eslint-disable-next-line
-  }, [])
+  // const a = useContext(noteContext)
+  // useEffect(() => {
+  //     a.update();
+  //     // eslint-disable-next-line
+  // }, [])
   return (
+    //{a.state.name} and in class {a.state.class}
       <div>
-          This is About {a.state.name} and he is in class {a.state.class}
+          This is About 
+          
       </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,22 +2,24 @@
 import NoteContext from "./noteContext";
 import { useState } from "react";
 const NoteState=(props)=>{
-    const s1 = {
-        "name":"Mariya",
-        "class":"6a"
-    }
-    const [state, setstate] = useState(s1)
-    const update = ()=>{
-        setTimeout(()=>{
-            setstate({
-                "name":"aariya",
-        "class":"aa6a"
-            })
-        },1000);
+    // const s1 = {
+    //     "name":"Mariya",
+    //     "class":"6a"
+    // }
+    // const [state, setstate] = useState(s1)
+    // const update = ()=>{
+    //     setTimeout(()=>{
+    //         setstate({
+    //             "name":"aariya",
+    //     "class":"aa6a"
+    //         })
+    //     },1000);
 
-    }
+    // }
+
+    //{{state,update}}
  return(
-   <NoteContext.Provider value= {{state,update}}>
+   <NoteContext.Provider value= {{}}>
     {props.children}
    </NoteContext.Provider>
  )
